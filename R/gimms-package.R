8c772dd065d2868cc4082a765ac9c182e0942017
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-10-06 16:36:17 +0200

    Inserted @import statements.

---
+++ R/gimms-package.R
@@ -0,0 +1,22 @@
+#' Download and Process GIMMS3g Data
+#'
+#' Set of functions to retrieve list of available GIMMS files, download and
+#' extract compressed data, and import binary files into R as standard
+#' 'Raster*' objects.
+#'
+#' @name gimms-package
+#' @aliases gimmspackage
+#' @docType package
+#' @title Download and Process GIMMS3g Data
+#' @author Florian Detsch
+#'
+#' @import methods raster
+#'
+#' @references Pinzon, JE & Tucker, CJ (2014). A Non-Stationary 1981–2012 AVHRR
+#' NDVI3g Time Series. Remote Sensing, 6(8), 6929-6960. Available online at
+#' \url{http://www.mdpi.com/2072-4292/6/8/6929/htm}.
+#'
+#' @keywords package
+#'
+NULL
+#'
