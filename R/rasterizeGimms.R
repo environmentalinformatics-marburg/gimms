1cdd2dc8debec8c571285d9704895859f75a5151
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-12-16 12:34:30 +0100

    Enabled parallel functionality.

--- R/rasterizeGimms.R
+++ R/rasterizeGimms.R
@@ -18,6 +18,8 @@
 #' @param remove_header Logical. If \code{FALSE} (default), the header file
 #' specified in 'header' or, if not specified, created internally via
 #' \code{\link{createHeader}} will be removed after all operations have finished.
+#' @param cores Integer. Number of cores for parallel computing. If 'filename'
+#' is not specified, the parallel option is automatically disabled.
 #' @param filename Character. Optional output filename, see
 #' \code{\link{writeRaster}}.
 #' @param ... Further arguments passed on to \code{\link{writeRaster}}.
@@ -34,14 +36,14 @@
 #'
 #' @examples
 #' \dontrun{
-#' ## Destination folder for data download
+#' ## Download sample data
 #' gimms_dir <- paste0(getwd(), "/data")
 #'
-#' ## Download GIMMS NDVI3g binary data from 2000-2005
-#' gimms_files <- downloadGimms(x = 2000, y = 2005, dsn = gimms_dir)
+#' gimms_files <- downloadGimms(x = as.Date("2000-01-01"),
+#'                              y = as.Date("2000-06-30"), dsn = gimms_dir)
 #'
-#' ## Rasterize downloaded GIMMS files from January and February 2000
-#' gimms_raster <- rasterizeGimms(x = gimms_files[1:4], remove_header = TRUE)
+#' ## Rasterize files
+#' gimms_raster <- rasterizeGimms(x = gimms_files, remove_header = TRUE)
 #'
 #' plot(gimms_raster)
 #' }
@@ -54,6 +56,7 @@ rasterizeGimms <-   function(x,
                              nodata2na = TRUE,
                              scaling = TRUE,
                              remove_header = FALSE,
+                             cores = 1L,
                              filename = '',
                              ...) {
 
@@ -61,44 +64,104 @@ rasterizeGimms <-   function(x,
   if (length(x) != length(filename) & all(nchar(filename)) > 0)
     stop("Parameters 'x' and 'filename' are of unequal length.")
 
-  ## loop over files in 'x'
-  ls_rst <- lapply(1:length(x), function(i) {
+  ### single core --------------------------------------------------------------
 
-    # import binary data as 'RasterLayer' and flip
-    if (is.null(header))
-      header <- createHeader(x[i])
+  if (cores == 1L | nchar(filename[1]) == 0) {
 
-    rst <- raster::raster(x[i])
-    rst <- raster::t(rst)
+    ## loop over files in 'x'
+    ls_rst <- lapply(1:length(x), function(i) {
 
-    # set extent and projection
-    ext <- raster::extent(c(-180 + 1/24, 180 - 1/24, -90 + 1/24, 90 - 1/24))
-    rst <- raster::setExtent(rst, ext)
-    projection(rst) <- "+init=epsg:4326"
+      # import binary data as 'RasterLayer' and flip
+      if (is.null(header))
+        header <- createHeader(x[i])
 
-    # discard 'water-mask' pixels (optional)
-    if (water2na)
-      rst[rst[] == -10000] <- NA
+      rst <- raster::raster(x[i])
+      rst <- raster::t(rst)
 
-    # discard 'nodata-mask' pixels (optional)
-    if (nodata2na)
-      rst[rst[] == -5000] <- NA
+      # set extent and projection
+      ext <- raster::extent(c(-180 + 1/24, 180 - 1/24, -90 + 1/24, 90 - 1/24))
+      rst <- raster::setExtent(rst, ext)
+      raster::projection(rst) <- "+init=epsg:4326"
 
-    # scale values (optional)
-    if (scaling)
-      rst <- rst / 10000
+      # discard 'water-mask' pixels (optional)
+      if (water2na)
+        rst[rst[] == -10000] <- NA
 
-    # store output (optional)
-    if (length(filename) >= i & nchar(filename[i]) > 0)
-      rst <- raster::writeRaster(rst, filename = filename[i], ...)
+      # discard 'nodata-mask' pixels (optional)
+      if (nodata2na)
+        rst[rst[] == -5000] <- NA
 
-    # if not otherwise specified, remove temporary header file
-    if (remove_header)
-      file.remove(header)
+      # scale values (optional)
+      if (scaling)
+        rst <- rst / 10000
 
-    # return raster
-    return(rst)
-  })
+      # store output (optional)
+      if (length(filename) >= i & nchar(filename[i]) > 0)
+        rst <- raster::writeRaster(rst, filename = filename[i], ...)
+
+      # if not otherwise specified, remove temporary header file
+      if (remove_header)
+        file.remove(header)
+
+      # return raster
+      return(rst)
+    })
+
+
+    ### multi-core -------------------------------------------------------------
+
+  } else {
+
+    ## initialize cluster
+    cl <- parallel::makePSOCKcluster(cores)
+
+    ## export required variables
+    parallel::clusterExport(cl, c("x", "header", "water2na", "nodata2na",
+                                  "scaling", "remove_header", "filename"),
+                            envir = environment())
+
+    ## loop over files in 'x'
+    ls_rst <- parallel::parLapply(cl, 1:length(x), function(i) {
+
+      # import binary data as 'RasterLayer' and flip
+      if (is.null(header))
+        header <- createHeader(x[i])
+
+      rst <- raster::raster(x[i])
+      rst <- raster::t(rst)
+
+      # set extent and projection
+      ext <- raster::extent(c(-180 + 1/24, 180 - 1/24, -90 + 1/24, 90 - 1/24))
+      rst <- raster::setExtent(rst, ext)
+      raster::projection(rst) <- "+init=epsg:4326"
+
+      # discard 'water-mask' pixels (optional)
+      if (water2na)
+        rst[rst[] == -10000] <- NA
+
+      # discard 'nodata-mask' pixels (optional)
+      if (nodata2na)
+        rst[rst[] == -5000] <- NA
+
+      # scale values (optional)
+      if (scaling)
+        rst <- rst / 10000
+
+      # store output (optional)
+      if (length(filename) >= i & nchar(filename[i]) > 0)
+        rst <- raster::writeRaster(rst, filename = filename[i], ...)
+
+      # if not otherwise specified, remove temporary header file
+      if (remove_header)
+        file.remove(header)
+
+      # return raster
+      return(rst)
+    })
+
+    # deregister parallel backend
+    parallel::stopCluster(cl)
+  }
 
   ## return 'RasterLayer' (single file in 'x') or 'RasterStack'
   if (length(ls_rst) == 1) {
