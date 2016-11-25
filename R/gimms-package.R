eee6e13b048de5ea4bae0a36f5bdece47415eaa6
fdetsch <florian.detsch@staff.uni-marburg.de>
2016-11-25 07:10:52 +0100

    Update: removed non-required dependencies; added documentation for 'baleNDVI3g' dataset.

--- R/gimms-package.R
+++ R/gimms-package.R
@@ -13,18 +13,30 @@
 #' @title Download and Process GIMMS3g Data
 #' @author Florian Detsch
 #'
-#' @import methods raster parallel foreach
-#' @importFrom doParallel registerDoParallel
+#' @import methods raster parallel
 #' @importFrom RCurl getURL
 #' @importFrom Kendall MannKendall
 #' @importFrom zyp zyp.trend.vector
 #' @importFrom utils download.file
 #'
-#' @references Pinzon, JE & Tucker, CJ (2014). A Non-Stationary 1981-2012 AVHRR
+#' @references
+#' Pinzon, JE & Tucker, CJ (2014). A Non-Stationary 1981-2012 AVHRR
 #' NDVI3g Time Series. Remote Sensing, 6(8), 6929-6960. Available online at
 #' \url{http://www.mdpi.com/2072-4292/6/8/6929/htm}.
+#'
+#' Pinzon, JE & Tucker, CJ (2016). A Non-Stationary 1981-2015 AVHRR
+#' NDVI3g.v1 Time Series: an update. In preparation for submission to Remote
+#' Sensing.
 #'
 #' @keywords package
 #'
 NULL
 #'
+#' @docType data
+#' @name baleNDVI3g
+#' @title Bale Mountains NDVI3g.v1
+#' @description Bale Mountains NDVI3g.v1
+#' @details This dataset contains NDVI3g.v1 observations for the Bale Mountains
+#' National Park, southern Ethiopia (Jul 1981 to Dec 2015).
+#' @format \code{raster::RasterStack}
+NULL
