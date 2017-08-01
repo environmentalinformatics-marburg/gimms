b1c784151cecdebb1e03a3a84450f5bd4c70d231
fdetsch <florian.detsch@staff.uni-marburg.de>
2017-08-01 11:23:54 +0200

    Initial upload: **testthat** suite of functions; bumped version and modified e-mail address.

---
+++ tests/testthat.R
@@ -0,0 +1,5 @@
+## this file has been created automatically by devtools::use_testthat()
+library(testthat)
+library(gimms)
+
+test_check("gimms")
---
+++ tests/testthat/test-updateInventory.R
@@ -0,0 +1,42 @@
+context("Update file inventory")
+
+## Code adopted from an answer by Joris Meys on StackOverflow
+## (https://stackoverflow.com/questions/7012796/ping-a-website-in-r), see
+## http://manpages.ubuntu.com/manpages/xenial/man8/ping.8.html for further
+## options. Arguments 'interval' and 'deadline' are currently disabled when
+## function is run on Windows.
+ping = function(x, count = 1L, interval = 1, deadline = 5, ...) {
+
+  os = Sys.info()[["sysname"]]
+  cnt = ifelse(os == "Windows", "-n", "-c")
+  out = system2("ping", paste(cnt, count, if (os != "Windows") {
+    paste("-i", interval, "-w", deadline)
+  }, x), stdout = FALSE, stderr = FALSE, ...)
+
+  if (os == "Windows") {
+    return(out == 0) # 1 = 'Ping request could not find host'
+  } else {
+    return(out %in% 0:1)  # 1 = 'Host found, but not all packages could be sent'
+                          # 2 = 'Ping request could not find host'
+  }
+}
+
+test_that("serverPath() of ECOCAST is reachable", {
+  for (i in 0:1) {
+    x = serverPath(version = i)
+    x = paste0(x, "/00FILE-LIST.txt")
+    expect_is(readLines(x), "character")
+
+    y = gsub("ecocast", "ecocase", x)
+    expect_error(suppressWarnings(readLines(y)), "cannot open the connection")
+  }
+})
+
+test_that("serverPath() of NASANEX is reachable", {
+  x = serverPath("nasanex")
+  x = gsub("https://", "", x)
+  expect_true(ping(x, count = 3L, interval = .5))
+
+  y = gsub("s3", "s4", x)
+  expect_false(ping(y, count = 3L, interval = .5))
+})
