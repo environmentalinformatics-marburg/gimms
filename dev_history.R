9224805bf208f0ad6e0209858dcf225b56823586
fdetsch <fdetsch@web.de>
2021-07-27 16:43:36 +0200

    added code for file download and rasterization

--- dev_history.R
+++ dev_history.R
@@ -1,5 +1,11 @@
 # 2021-07-27 ====
 
+## code related to issue #4, available online:
+## https://github.com/environmentalinformatics-marburg/gimms/issues/4
+
+
+### `updateInventory()` ----
+
 onl1 = updateInventory() # `poles`, fails
 onl2 = updateInventory(
   server = "nasanex"
@@ -15,4 +21,23 @@ debug(
 onl1 = updateInventory()
 undebug(
   gimms:::updatePoles
+)
+
+
+### `downloadGimms()` ----
+
+(
+  files_1985 = downloadGimms(
+    x = as.Date("1985-01-01")
+    , y = as.Date("1985-12-31")
+  )
+)
+
+
+### `rasterizeGimms()` ----
+
+(
+  raster_1985 = rasterizeGimms(
+    files_1985
+  )
 )
